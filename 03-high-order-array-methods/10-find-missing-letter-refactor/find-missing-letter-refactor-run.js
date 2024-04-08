6f6780b4f20ad6d3100fda80d9b7aa525527fdaa
Zouhair Sahtout <zouhairsahtout66@gmail.com>
2024-04-08 12:38:40 -0400

    Solved the last missing challenge in high order array methods

--- 03-high-order-array-methods/10-find-missing-letter-refactor/find-missing-letter-refactor-run.js
+++ 03-high-order-array-methods/10-find-missing-letter-refactor/find-missing-letter-refactor-run.js
@@ -1,5 +1,9 @@
 const findMissingLetter = require('./find-missing-letter-refactor');
 
-const result = findMissingLetter(['a', 'b', 'c', 'e']);
+const result1 = findMissingLetter(['a', 'b', 'c', 'e']);
+const result2 = findMissingLetter(['m', 'n', 'o', 'q', 'r']);
 
-console.log(result);
+console.table({
+    "['a', 'b', 'c', 'e']": result1,
+    "['m', 'n', 'o', 'q', 'r']": result2
+});
--- 03-high-order-array-methods/10-find-missing-letter-refactor/find-missing-letter-refactor.js
+++ 03-high-order-array-methods/10-find-missing-letter-refactor/find-missing-letter-refactor.js
@@ -1,3 +1,38 @@
-function findMissingLetter() {}
+function findMissingLetter(arr) {
+    let start = arr.at(0).charCodeAt(0);
+
+    const missingCharCode = arr.reduce(function(missing, char) {
+        const current = char.charCodeAt(0);
+
+        if(current - start > 1 && missing === null) {
+            missing = start + 1;
+        }
+
+        start = current;
+        return missing;
+
+    }, null);
+
+    return missingCharCode ? String.fromCharCode(missingCharCode) : '';
+}
 
 module.exports = findMissingLetter;
+
+
+
+/*
+    1) Understanding the problem
+    - Returns the missing letter from an increasing letters array
+
+    2) Break it into sub-problem
+    - The function will receive an array of letters increasingly
+    - The first letter of the array gonna be the starter number
+    - That starter code number gonna store in separate variable
+    - For each iteration we are gonna check:
+    - If the current code - starter > 1 and there's no missing yet
+    -       Set the missing to the starter + 1, instead of null
+    -       This gonna be the the missing character code
+
+    - Set the starter code to the current code, for the next check
+    - And return the the missing character code every time
+*/
